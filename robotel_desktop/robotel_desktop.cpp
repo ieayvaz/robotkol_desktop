dbc47a31efcb49b0f8f20625ca9ef3b0cd64b3eb
collorpus@gmail.com <collorpus@gmail.com>
2021-05-26 15:11:25 +0300

    Proje dosyası ekle.

---
+++ robotel_desktop/robotel_desktop.cpp
@@ -0,0 +1,12 @@
+﻿// robotel_desktop.cpp: Uygulamanın giriş noktasını tanımlar.
+//
+
+#include "robotel_desktop.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
